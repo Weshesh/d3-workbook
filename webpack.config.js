20ba93eb9c14ba092194b61ee8a92be007372f04
Andi Angel <andi.angel@boheemia.ee>
2022-02-19 21:09:51 +0200

    Modify webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
   }
 }
